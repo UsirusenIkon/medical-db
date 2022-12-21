8cbfe7167b087f18fbb0c0efecd994e73bcdecfe
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-12-21 18:41:49 +0500

    Adding new tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,23 @@ CREATE TABLE invoices (
   payed_at TIMESTAMP,
   medical_history_id INT
 );
+
+CREATE TABLE treatments (
+  id BIGSERIAL PRIMARY KEY,
+  type VARCHAR(200) NOT NULL,
+  name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE invoice_items (
+  id BIGSERIAL PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id INT REFERENCES invoices(id) NOT NULL,
+  treatement_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE medical_histories_has_treatments (
+  medical_history_id INT REFERENCES medical_histories(id),
+  treatment_id INT REFERENCES treatments(id)
+);
